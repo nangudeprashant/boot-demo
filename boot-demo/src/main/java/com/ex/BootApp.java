addf664d86a9d7337725bae49abb854920b13589
nangudeprashant <30728347+nangudeprashant@users.noreply.github.com>
2018-01-18 13:34:58 +0530

    First commit.

---
+++ boot-demo/src/main/java/com/ex/BootApp.java
@@ -0,0 +1,19 @@
+package com.ex;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BootApp extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootApp.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BootApp.class, args);
+	}
+
+}
